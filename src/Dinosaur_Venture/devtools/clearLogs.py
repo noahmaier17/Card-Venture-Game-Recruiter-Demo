38261a0450523e83cf92c83e706a85447462010f
noahm17 <noahm17@cs.washington.edu>
2025-09-05 14:54:01 -0700

    Added log clearing devtool

---
+++ src/Dinosaur_Venture/devtools/clearLogs.py
@@ -0,0 +1,19 @@
+import os
+from send2trash import send2trash
+
+directory_path = "Logs/"
+
+def code():
+    ## Gets all the files
+    files = []
+    for entry in os.listdir(directory_path):
+        full_path = os.path.join(directory_path, entry)
+        if os.path.isfile(full_path):
+            files.append(directory_path + entry)
+    print(files)
+
+    ## Sends them to the trash
+    send2trash(files)
+
+if __name__ == '__main__':
+    code()
