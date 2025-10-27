1ca7d4f6331e6abd3d24665e7b6fa86a88aa6d6f
noahm17 <noahm17@cs.washington.edu>
2025-10-26 17:12:10 -0700

    Working towards hosting web application on Render

--- web_app/app.py
+++ web_app/app.py
@@ -125,5 +125,4 @@ def add_card():
 
 ## ----- Main Guard ------
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
