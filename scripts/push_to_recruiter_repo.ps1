2d14b5cff75fce8492a3c13a1f1c8ff8458e53a7
noahm17 <noahm17@cs.washington.edu>
2025-12-03 18:12:21 -0800

    Testing PowerShell script

--- scripts/push_to_recruiter_repo.ps1
+++ scripts/push_to_recruiter_repo.ps1
@@ -10,8 +10,12 @@ if (Test-Path ".git/MERGE_HEAD") {
     git merge main
 }
 
+git status
+
 # Removing git tracking in the recruiter-demo branch that I do not want pushed
 git rm -r '.\Storage of Deprecated Things'
+
+git status
 
 # Detects if this merge had any conflicts
 if (git ls-files -u) {
