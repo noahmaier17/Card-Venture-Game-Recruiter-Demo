a43c7ecddfd49457723d64df18508bc7f1981a5a
noahm17 <noahm17@cs.washington.edu>
2025-12-03 18:00:08 -0800

    Added PowerShell script for pushing to recruiter repo

---
+++ scripts/push_to_recruiter_repo.ps1
@@ -0,0 +1,29 @@
+# Checks out the recruiter-demo branch on this repository
+Write-Host ">> Checkout recruiter-demo branch" -ForegroundColor Cyan
+git checkout recruiter-demo
+
+# Merges the main branch to the recruiter-demo branch
+Write-Host ">> Merge main into recruiter-demo branch" -ForegroundColor Cyan
+git merge main
+
+# Removing git tracking in the recruiter-demo branch that I do not want pushed
+git rm -r '.\Storage of Deprecated Things'
+
+<#
+# Detects if this merge had any conflicts
+if (git ls-files -u) {
+    Write-Host ">> Merge conflict; resolve and then re-run this script" -ForegroundColor Red
+    exit 1
+}
+
+# Pushs to the remote repository
+Write-Host ">> Push recruiter-demo to remote 'demo' repo" -ForegroundColor Cyan
+git push demo recruiter-demo
+
+# Checks out the main branch
+Write-Host ">> Checkout main branch" -ForegroundColor Cyan
+git checkout main
+
+# States the script is complete
+Write-Host ">> Script complete" -ForegroundColor Cyan
+#>
