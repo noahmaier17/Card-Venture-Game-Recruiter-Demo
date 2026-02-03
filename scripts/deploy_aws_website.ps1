18b854d1149b6328a9e0ddcd59e7aa7d88be5d5c
noahm17 <noahm17@cs.washington.edu>
2026-02-03 14:48:13 -0800

    Added AWS Lambda webhosting

---
+++ scripts/deploy_aws_website.ps1
@@ -0,0 +1,22 @@
+# ======================================================
+# Script: Updates remote-hosted Lambda website.
+# ======================================================
+
+# Activates the virtual environment that zappa requires
+Write-Host ">> Activating virtual environment" -ForegroundColor Cyan
+& ".\.venv\Scripts\Activate.ps1"
+
+# Runs the pytest suite to ensure correctness
+Write-Host ">> Running pytest suite" -ForegroundColor Cyan
+pytest
+
+# Logs into aws
+Write-Host ">> Logging into AWS" -ForegroundColor Cyan
+aws login
+
+# Updating zappa deployment
+Write-Host ">> Updating zappa deployment" -ForegroundColor Cyan
+zappa update dev
+
+# States the script is complete
+Write-Host ">> Script complete" -ForegroundColor Cyan
