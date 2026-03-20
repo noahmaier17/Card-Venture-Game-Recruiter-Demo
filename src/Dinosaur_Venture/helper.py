b5f4139ee57e51333946f80f45953f254a4ef414
noahm17 <noahm17@cs.washington.edu>
2026-03-19 18:17:01 -0700

    feat: made 'HP' red colored

--- src/Dinosaur_Venture/helper.py
+++ src/Dinosaur_Venture/helper.py
@@ -834,7 +834,7 @@ def colorize_AsCodes(text: str) -> list[tuple[str, Optional["colorize_AsCodes.Co
                                   ColorizeCode(back_red=True), return_sequence)
             appendNewColorizeCode(None, "'s",
                                   None, return_sequence)
-        elif word in ["Band", "Bands", "EXCEPT"]:
+        elif word in ["Band", "Bands", "EXCEPT", "HP"]:
             appendNewColorizeCode(cached_blank_sequence, word,
                                   ColorizeCode(fore_red=True, style_bright=True), return_sequence)
         elif word in ["Triggered", "Special", "Gimmick"]:
@@ -984,7 +984,7 @@ def colorize(text: str) -> str:
             returnText += Back.RED + word + Back.RESET
         elif word == "Enemy's":
             returnText += Back.RED + "Enemy" + Back.RESET + "'s"
-        elif word in ["Band", "Bands", "EXCEPT"]:
+        elif word in ["Band", "Bands", "EXCEPT", "HP"]:
             returnText += Fore.RED + Style.BRIGHT + word + Fore.WHITE + Style.NORMAL
         elif word in ["Triggered", "Special", "Gimmick"]:
             returnText += Fore.YELLOW + word + Fore.WHITE
